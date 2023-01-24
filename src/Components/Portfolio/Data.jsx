e96db1cb10b3b0eb35b77066ad0c67a99bb8bc35
Mugesh <mugeshkumark22@gmail.com>
2023-01-24 20:11:37 +0530

    portfolio changes

--- src/Components/Portfolio/Data.jsx
+++ src/Components/Portfolio/Data.jsx
@@ -1,5 +1,10 @@
 import work1 from "../../assests/Work1.png";
 import work2 from "../../assests/Work2.png";
+import work3 from "../../assests/work3.jpg";
+import work4 from "../../assests/work4.jpg";
+
+
+
 
 
 
@@ -32,15 +37,29 @@ export const projectsData = [
     techStack3: "Mongodb",
     techStack4: "Bootstrap",
 },
-// {
-//     id:3,
-//     image: work3,
-//     title: "Project3"
-// },
-// {
-//     id:4,
-//     image: work4,
-//     title: "Project4"
-// }
+{
+    id:3,
+    image: work3,
+    title: "Daily Notes App",
+    demo:"https://notesapp2211.netlify.app/",
+    git: "https://github.com/Mugesh2297/Notes-App-Front-End",
+    des:"Notes app helps to save the daily note kind of personal diary, which helps us to save notes by login with email and password after registration helps to use the app in person",
+    techStack1: "Reactjs",
+    techStack2: "Expressjs",
+    techStack3: "Mongodb",
+    techStack4: "Bootstrap",
+},
+{
+    id:4,
+    image: work4,
+    title: "Pizza Delivery App",
+    demo:"https://pizzaapp22.netlify.app/",
+    git: "https://github.com/Mugesh2297/Pizza_App_FrontEnd",
+    des:"Pizza Delivery App which help to order online, add to cart function has been added and implemented the online payment options and has seperate admin login to access through the users and order details",
+    techStack1: "Reactjs",
+    techStack2: "Expressjs",
+    techStack3: "Mongodb",
+    techStack4: "Bootstrap",
+}
 
 ]
